a5fe928feeafe13e5cd5ece4052f15172e47c3f6
yy.chung <yy.chung@linewalks.com>
2021-12-17 16:49:05 +0900

    improvement(config_content_manager): separate getter, setter functions

--- config_parser.py
+++ config_parser.py
@@ -1,15 +1,26 @@
-import argparse
 import configparser
 
 dummy_section = "[DEFAULT]\n"
 
-class ConfigFileParser:
-  def __init__(self, path):
-    self.config_parser = configparser.ConfigParser(allow_no_value=True)
-    with open(path) as config_file:
-      content = f"{dummy_section}{config_file.read()}"
 
-    self.config_parser.read_string(content)
+class ConfigReader:
+  def __init__(self, filepath):
+    self.filepath = filepath
+    self.config_parser = configparser.ConfigParser(allow_no_value=True)
+
+    with open(filepath) as config_file:
+      content = config_file.read()
+    try:
+      self.config_parser.read_string(content)
+    except configparser.MissingSectionHeaderError:
+      content = f"{dummy_section}{content}"
+      self.config_parser.read_string(f"{dummy_section}{content}")
+
+  def _set_filepath(self, filepath):
+    self.filepath = filepath
+  
+  def get_filepath(self):
+    return filepath
 
   def get_key_list(self):
     return self.config_parser.defaults().keys()
