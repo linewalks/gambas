2a4ea8c5f4ffc6cc6fc52d39542e1a00ab944a2d
yy.chung <yy.chung@linewalks.com>
2021-12-21 18:23:11 +0900

    test: add validator test

---
+++ tests/test_validator.py
@@ -0,0 +1,28 @@
+import pytest
+from validator import ConfigValidator
+
+
+class TestValidator:
+  @pytest.fixture(scope="class")
+  def default_config_path(self):
+    return "tests/files/default_config.cfg"
+    
+  @pytest.fixture(scope="class")
+  def correct_config_path(self):
+    return "tests/files/test_correct_config.cfg"
+    
+  @pytest.fixture(scope="class")
+  def missing_config_path(self):
+    return "tests/files/test_missing_config.cfg"
+
+  def test_validate_correct_config_file(self, default_config_path, correct_config_path):
+    config_validator = ConfigValidator(default_config_path, correct_config_path)
+    try:
+      config_validator.check_exist_keys()
+    except Exception as e:
+      assert False, e
+  
+  def test_validate_missing_config_file(self, default_config_path, missing_config_path):
+    config_validator = ConfigValidator(default_config_path, missing_config_path)
+    with pytest.raises(KeyError):
+      config_validator.check_exist_keys()
