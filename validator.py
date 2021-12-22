62d4f0998be520dd0a61812edce20a3341374fbd
yy.chung <yy.chung@linewalks.com>
2021-12-22 16:55:57 +0900

    improvement: raise just warning when is_error is False

--- validator.py
+++ validator.py
@@ -8,7 +8,7 @@ class ConfigValidator:
     self.default_keys = ConfigContentManager(self.__default_filepath).get_key_list()
     self.target_keys = ConfigContentManager(self.__target_filepath).get_key_list()
 
-  def check_exist_keys(self):
+  def check_exist_keys(self, is_error=True):
     cand_key_list = [
         cand_key
         for cand_key in self.default_keys
@@ -16,6 +16,7 @@ class ConfigValidator:
     ]
 
     if cand_key_list:
-      raise KeyError(f"{', '.join(cand_key_list)} not in {self.__target_filepath}")
+      sentence = f"{', '.join(cand_key_list)} not in {self.__target_filepath}"
+      raise KeyError(sentence) if is_error else Warning(sentence)
     else:
       print(f"There are all keys in a target config file {self.__target_filepath}")
