17af2787942df80d2abe99ea3003352d602ec8fe
yy.chung <yy.chung@linewalks.com>
2021-12-22 14:24:15 +0900

    improvement: test update to make and remove config files in test class

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -9,18 +9,56 @@ import pytest
 from validator import ConfigValidator
 
 
+TEST_CONFIG_FOLDER = os.path.join(BASE_DIR, "tests", "files")
+
+TEST_PAIR = {
+    "TEST_USER": "user",
+    "TEST_LUNCH": "gambas",
+    "TEST_DINNER": "ssam"
+}
+
+
 class TestValidator:
   @pytest.fixture(scope="class")
-  def default_config_path(self):
-    return os.path.join(BASE_DIR "tests", "files", "default_config.cfg")
+  def test_file_folder(self):
+    if not os.path.isdir(TEST_CONFIG_FOLDER):
+      os.makedirs(TEST_CONFIG_FOLDER)
+    
+    yield TEST_CONFIG_FOLDER
+    os.rmdir(TEST_CONFIG_FOLDER)
+
+  @pytest.fixture(scope="class")
+  def default_config_path(self, test_file_folder):
+    filepath = os.path.join(test_file_folder, "default_config.cfg")
+
+    with open(filepath, "w") as f:
+      for test_key in TEST_PAIR.keys():
+        f.write(f"{test_key}=\n")
+
+    yield filepath
+    os.remove(filepath)
+
+  @pytest.fixture(scope="class")
+  def correct_config_path(self, test_file_folder):
+    filepath = os.path.join(test_file_folder, "correct_config.cfg")
+
+    with open(filepath, "w") as f:
+      for test_key, test_value in TEST_PAIR.items():
+        f.write(f'{test_key}="{test_value}"\n')
+
+    yield filepath
+    os.remove(filepath)
     
   @pytest.fixture(scope="class")
-  def correct_config_path(self):
-    return os.path.join(BASE_DIR, "tests", "files", "test_correct_config.cfg")
-    
-  @pytest.fixture(scope="class")
-  def missing_config_path(self):
-    return os.path.join(BASE_DIR, "tests", "files", "test_missing_config.cfg")
+  def missing_config_path(self, test_file_folder):
+    filepath = os.path.join(test_file_folder, "missing_config.cfg")
+
+    with open(filepath, "w") as f:
+      for test_key, test_value in list(TEST_PAIR.items())[:1]:
+        f.write(f'{test_key}="{test_value}"\n')
+  
+    yield filepath
+    os.remove(filepath)
 
   def test_validate_correct_config_file(self, default_config_path, correct_config_path):
     config_validator = ConfigValidator(default_config_path, correct_config_path)
