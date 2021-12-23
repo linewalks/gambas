60d5c5c04f085ffeb6033e38742c8490756dc757
yy.chung <yy.chung@linewalks.com>
2021-12-23 16:49:28 +0900

    chore: fix typo

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -12,7 +12,7 @@ class TestManager:
     """
     NOTE
     ConfigContentManager has features to parsing keys of a config file and return them as a list.
-    default_config.cfg were made of TEST_PAIR on conftest.py
+    default_config.cfg was made of TEST_PAIR on conftest.py
     Therefore, in this test, validate that default_config.cfg has all keys same with TEST_PAIR's
     """
     config_key_list = config_manager.get_key_list()
