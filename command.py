7c2d5ef12c57dff05b39c3495bfbc2b0d6d974de
yy.chung <yy.chung@linewalks.com>
2021-12-23 13:25:00 +0900

    refactor: set command with argparse as a class

---
+++ command.py
@@ -0,0 +1,24 @@
+import argparse
+from validator import ConfigValidator
+
+
+class Command:
+  def __init__(self):
+    self.parser = argparse.ArgumentParser(description="*** Config File Validator Option ***")
+    self.add_options()
+  
+  def add_options(self):
+    self.parser.add_argument("-d", "--default-path", type=str, help="Default config file path")
+    self.parser.add_argument("-t", "--target-path", type=str, help="Target config file path")
+    self.parser.add_argument(
+        "--no-error",
+        default=True,
+        dest="error",
+        action="store_false",
+        help="""Do not get error when some keys do not exists. just raise warning"""
+    )
+    self.args = self.parser.parse_args()
+
+  def run(self):
+    config_validator = ConfigValidator(self.args.default_path, self.args.target_path)
+    config_validator.check_exist_keys(self.args.error)
--- run.py
+++ run.py
@@ -1,19 +1,5 @@
-import argparse
-from validator import ConfigValidator
+from command import Command
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description="*** Config File Validator Option ***")
-  parser.add_argument("-d", "--default-path", type=str, help="Default config file path")
-  parser.add_argument("-t", "--target-path", type=str, help="Target config file path")
-  parser.add_argument(
-      "--no-error",
-      default=True,
-      dest="error",
-      action="store_false",
-      help="""Do not get error when some keys do not exists. just raise warning"""
-  )
-
-  args = parser.parse_args()
-
-  config_validator = ConfigValidator(args.default_path, args.target_path)
-  config_validator.check_exist_keys(args.error)
+  cmd = Command()
+  cmd.run()
