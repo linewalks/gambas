4d22aa10fae9326a5ab988ef3cbb0fb4b7ab51ab
yy.chung <yy.chung@linewalks.com>
2021-12-23 16:32:23 +0900

    chore: return the value directly

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -6,8 +6,7 @@ from manager import ConfigContentManager
 class TestManager:
   @pytest.fixture(scope="class")
   def config_manager(self, default_config_path):
-    cm = ConfigContentManager(default_config_path)
-    return cm
+    return ConfigContentManager(default_config_path)
 
   def test_get_key_list(self, config_manager):
     config_key_list = config_manager.get_key_list()
