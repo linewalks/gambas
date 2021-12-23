585508950e9ed894244ed1d9d2eb21d5ad1e8a0b
yy.chung <yy.chung@linewalks.com>
2021-12-23 16:44:42 +0900

    chore: add an explanation for the test in detail

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -9,6 +9,12 @@ class TestManager:
     return ConfigContentManager(default_config_path)
 
   def test_get_key_list(self, config_manager):
+    """
+    NOTE
+    ConfigContentManager has features to parsing keys of a config file and return them as a list.
+    default_config.cfg were made of TEST_PAIR on conftest.py
+    Therefore, in this test, validate that default_config.cfg has all keys same with TEST_PAIR's
+    """
     config_key_list = config_manager.get_key_list()
     config_key_upper_list = map(lambda x: x.upper(), config_key_list)
     assert set(TEST_PAIR.keys()) == set(config_key_upper_list)
