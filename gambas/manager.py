73e181f07c951ccf4ce13794bf30ba73b219ee51
yy.chung <yy.chung@linewalks.com>
2022-02-09 10:28:14 +0900

    chore: change the variable name

--- gambas/manager.py
+++ gambas/manager.py
@@ -26,16 +26,16 @@ class BaseContentManager(metaclass=ABCMeta):
 class ConfigContentManager(BaseContentManager):
   def __init__(self, filepath):
     super().__init__(filepath)
-    self.config = {}
+    self.config_dict = {}
     self._read_file()
 
   def _read_file(self):
     filepath = self.get_filepath()
     with open(filepath, mode="rb") as config_file:
-      exec(compile(config_file.read(), filepath, "exec"), None, self.config)
+      exec(compile(config_file.read(), filepath, "exec"), None, self.config_dict)
 
   def get_key_list(self):
-    return self.config.keys()
+    return self.config_dict.keys()
 
 
 class JsonContentManager(BaseContentManager):
